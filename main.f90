3dd31f3c5bfeed6c0235300d122bd68385090b4f
Shunsuke Sato <shunsuke.sato26@gmail.com>
2020-01-21 15:01:48 +0100

    Implemented for the real-time claculation of conductivity.

--- main.f90
+++ main.f90
@@ -15,7 +15,7 @@ module global_variables
   real(8),parameter :: fs = 1d0/0.024189d0
 
 ! Drude parameters
-  real(8) :: ne_dns, mass, gamma
+  real(8) :: ne_dns, mass, gamma, delta_gamma
 
 ! Physical system
   real(8) :: velocity_t
@@ -23,10 +23,16 @@ module global_variables
 ! time propagation
   integer :: nt
   real(8) :: Tprop, dt
+  real(8), allocatable :: jt(:)
+
+! frequency domain
+  integer,parameter :: nw = 1024
+  real(8),parameter :: wi = 1d0*ev, wf = 90d0*ev, dw = (wf-wi)/nw
+  complex(8) :: zsigma_w(0:nw), zsigma_gs_w(0:nw), zEw_store(0:nw)
 
 ! laser
   real(8),allocatable :: Eprobe(:)
-  real(8),allocatable :: Ipump(:)
+  real(8),allocatable :: Ipump(:), Ipump_int(:)
   real(8) :: Tpump, Tprobe_train, omega_probe, omega_pump, tdelay, t_offset
   
   
@@ -39,7 +45,8 @@ program main
 
   call set_parameter
   call calc_stationary
-  call set_laser
+!  call set_laser
+  call pump_probe_dynamics
 
 end program main
 !------------------------------------------------------------------------------!
@@ -50,11 +57,14 @@ subroutine set_parameter
 
   mass = 1d0
 !  ne_dns = (15d0*ev)**2*mass/(4d0*pi)
-  ne_dns = (7.5d0*ev)**2*mass/(4d0*pi)
+!  ne_dns = (7.5d0*ev)**2*mass/(4d0*pi)
+  ne_dns = (15.3d0*ev)**2*mass/(4d0*pi)
 
 
 !  gamma = 7.5d0*ev
-  gamma = 1d0/(30d0/0.024189d3)
+!  gamma = 1d0/(30d0/0.024189d3)
+  gamma = 0.5984d0*ev
+  delta_gamma = gamma*0.01d0
 
 
   Tprop = 100d0*fs
@@ -67,9 +77,6 @@ end subroutine set_parameter
 subroutine calc_stationary
   use global_variables
   implicit none
-  integer,parameter :: nw = 512
-  real(8),parameter :: wi = 0.1d0*ev, wf = 90d0*ev
-  real(8),parameter :: dw = (wf-wi)/nw
   real(8),parameter :: L_thick = 200d0*nm
   integer :: iw
   real(8) :: ww
@@ -104,11 +111,10 @@ subroutine calc_stationary
     abs_coeff = 2d0*aimag(zn)*ww/clight*cm
 
 
-    write(20,"(999e26.16e3)")ww/ev,transmission,abs_coeff,zeps,-aimag(1d0/zeps)
+    write(20,"(999e26.16e3)")ww/ev,transmission,abs_coeff,zsigma,zeps,-aimag(1d0/zeps)
   end do
   close(20)
   
-
 
 
 end subroutine calc_stationary
@@ -119,30 +125,39 @@ subroutine set_laser
   integer :: it
   real(8) :: tt, xx, ss
 
-  allocate(Eprobe(-1:nt+1), Ipump(-1:nt+1))
+  allocate(Eprobe(-1:nt+1), Ipump(-1:nt+1), Ipump_int(-1:nt+1))
   omega_pump = 1.5d0*ev
   omega_probe = omega_pump*27d0
   Tpump = 40d0*fs
   Tprobe_train = 10d0*fs
-  tdelay = 0d0*fs
+!  tdelay = 0d0*fs
 
   t_offset = min(-0.5d0*Tpump, -0.5d0*tprobe_train+tdelay)
 
   Ipump = 0d0
+  Ipump_int = 0d0
   Eprobe = 0d0
 ! pump
-  do it = 0, nt
+  do it = 0, nt+1
     tt = dt*it
     xx = tt + t_offset
     if(abs(xx) <= 0.5d0*Tpump)then
       Ipump(it) = cos(pi*xx/Tpump)**2
     end if
   end do
-  ss = sum(Ipump)*dt
+
+  ss = 0d0
+  do it = 0, nt+1
+    ss = ss + 0.5d0*(Ipump(it)+Ipump(it-1))*dt
+    Ipump_int(it) = ss
+  end do
+  ss = Ipump_int(nt+1)
+  Ipump_int = Ipump_int/ss
   Ipump = Ipump/ss
+  
 
 ! probe
-  do it = 0, nt
+  do it = 0, nt+1
     tt = dt*it
     xx = tt + t_offset-tdelay
     if(abs(xx) <= 0.5d0*Tprobe_train)then
@@ -151,16 +166,117 @@ subroutine set_laser
   end do
 
 
-  open(20,file='laser.dat')
-  do it = 0, nt
-    write(20,"(999e26.16e3)")dt*it,Ipump(it),Eprobe(it)
-  end do
-  close(20)
-
-
+!  open(20,file='laser.dat')
+!  do it = 0, nt
+!    write(20,"(999e26.16e3)")dt*it,Ipump(it),Eprobe(it)
+!  end do
+!  close(20)
 
 
 end subroutine set_laser
+!------------------------------------------------------------------------------!
+subroutine unset_laser
+  use global_variables
+  implicit none
+
+  deallocate(Eprobe, Ipump, Ipump_int)
+end subroutine unset_laser
+!------------------------------------------------------------------------------!
+subroutine pump_probe_dynamics
+  use global_variables
+  implicit none
+  integer :: ndelay, idelay
+  real(8) :: tdelay_ini, tdelay_fin
+  integer :: iw
+
+  allocate(jt(0:nt))
+
+  tdelay_ini = -10d0*fs
+  tdelay_fin =  10d0*fs
+  ndelay = 20
+
+! no-pump calculation
+  tdelay = 0d0
+  call set_laser
+  Ipump = 0d0; Ipump_int = 0d0
+  call calc_dynamics
+  call calc_conductivity
+  open(30,file='sigma_gs.out')
+  do iw = 0, nw
+    write(30,"(999e26.16e3)")(wi+dw*iw)/ev,zsigma_w(iw),abs(zEw_store(iw))**2
+  end do
+  close(30)
+  call unset_laser
+
+
+!  do idelay = 0, ndelay
+!    tdelay = t_delay_ini + idelay*(t_delay_fin-t_delay_ini) /ndelay
+!  end do
+
+end subroutine pump_probe_dynamics
+!------------------------------------------------------------------------------!
+subroutine calc_dynamics
+  use global_variables
+  implicit none
+  integer :: it
+
+  jt = 0d0
+  velocity_t = 0d0
+  jt(0) = velocity_t*ne_dns/mass
+
+  do it = 0, nt
+    call dt_evolve(it)
+    jt(it+1) = velocity_t*ne_dns/mass
+  end do
+
+
+end subroutine calc_dynamics
+!------------------------------------------------------------------------------!
+subroutine dt_evolve(it)
+  use global_variables
+  implicit none
+  integer, intent(in) :: it
+  real(8) :: gamma_eff, gamma_n, gamma_o
+
+  gamma_o = gamma + delta_gamma*Ipump_int(it)
+  gamma_n = gamma + delta_gamma*Ipump_int(it+1)
+  
+  gamma_eff = 0.5d0*(gamma_n + gamma_o)
+  
+  velocity_t = velocity_t*exp(-dt*gamma_eff) &
+    +0.5d0*dt*(Eprobe(it+1) + Eprobe(it) )/mass
+
+end subroutine dt_evolve
+!------------------------------------------------------------------------------!
+subroutine calc_conductivity
+  use global_variables
+  implicit none
+  integer :: iw, it
+  real(8) :: ww,tt
+  complex(8) :: zjw, zEw, zfact
+
+  zsigma_w = 0d0
+
+  do iw = 0, nw
+    ww = wi + dw*iw
+
+    zjw = 0d0
+    zEw = 0d0
+    do it = -1,nt+1
+      tt = dt*it
+      zfact = exp(zI*ww*tt)
+      zjw = zjw + jt(it)    *zfact
+      zEw = zEw + Eprobe(it)*zfact
+    end do
+    zsigma_w(iw) = zjw/zEw
+    zEw_store(iw) = zEw*dt
+
+  end do
+
+
+
+end subroutine calc_conductivity
+!------------------------------------------------------------------------------!
 !------------------------------------------------------------------------------!
 !------------------------------------------------------------------------------!
 !------------------------------------------------------------------------------!
